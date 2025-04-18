22b02dda53ed3063e01541047fa4f8138a97e13b
Loris <loris.accordino@gmail.com>
2025-04-18 20:12:52 +0200

    Aggiungere i file di progetto.

---
+++ SignalManipulator/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace SignalManipulator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
