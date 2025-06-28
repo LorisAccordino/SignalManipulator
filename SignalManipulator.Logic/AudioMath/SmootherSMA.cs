ac28b6f99a2ccc07e94aa820c52cba826019f94e
Loris <loris.accordino@gmail.com>
2025-06-28 18:46:01 +0200

    Added smoothers

---
+++ SignalManipulator.Logic/AudioMath/SmootherSMA.cs
@@ -0,0 +1,36 @@
+﻿using SignalManipulator.Logic.AudioMath;
+using System.Collections.Generic;
+using System;
+
+public class SmootherSMA : Smoother
+{
+    private readonly Queue<double[]> history = new Queue<double[]>();
+    private readonly int maxHistory;
+
+    public SmootherSMA(int historyLength)
+    {
+        if (historyLength < 1)
+            throw new ArgumentOutOfRangeException(nameof(historyLength), "History length must be ≥ 1");
+
+        maxHistory = historyLength;
+    }
+
+    public override double[] Smooth(double[] input)
+    {
+        history.Enqueue((double[])input.Clone());
+        if (history.Count > maxHistory)
+            history.Dequeue();
+
+        int length = input.Length;
+        double[] result = new double[length];
+
+        foreach (var arr in history)
+            for (int i = 0; i < length; i++)
+                result[i] += arr[i];
+
+        for (int i = 0; i < length; i++)
+            result[i] /= history.Count;
+
+        return result;
+    }
+}
